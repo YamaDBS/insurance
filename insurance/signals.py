d550dd28b1194fdab47f13db3f7711a19297f0b0
mykolamateichuk <mateichukmykola@gmail.com>
2023-12-17 13:45:02 +0200

    Add agent and client creation on users creation

--- insurance/signals.py
+++ insurance/signals.py
@@ -1,14 +1,16 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from insurance.models import Client
+from insurance.models import Client, Agent
 from user.models import User
 
 
 @receiver(post_save, sender=User)
 def create_client_after_user_created(sender, instance, created, **kwargs):
     if created and instance.is_client:
-        Client.objects.create()
+        Client.objects.create(user=instance)
+    if created and instance.is_agent:
+        Agent.objects.create(user=instance)
 
 
 post_save.connect(create_client_after_user_created, sender=User)
