b5dc40bc067d50dc02b6e4554e184e6a2cb6585b
mykolamateichuk <mateichukmykola@gmail.com>
2023-12-17 19:23:16 +0200

    Fix issues

--- insurance/permissions.py
+++ insurance/permissions.py
@@ -1,10 +1,12 @@
 from rest_framework import permissions
 
 
-class IsAgentUser(permissions.BasePermission):
+class IsAgentUserOrIsAdminUser(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return bool(
-            (request.user and request.user.is_authenticated
+            ((request.user and request.user.is_authenticated
              and request.user.is_agent)
+             or (request.user and request.user.is_authenticated
+                 and request.user.is_staff))
         )
--- insurance/signals.py
+++ insurance/signals.py
@@ -1,16 +1,16 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_init
 from django.dispatch import receiver
 
 from insurance.models import Client, Agent
 from user.models import User
 
 
-@receiver(post_save, sender=User)
+@receiver(post_init, sender=User)
 def create_client_after_user_created(sender, instance, created, **kwargs):
     if created and instance.is_client:
         Client.objects.create(user=instance)
     if created and instance.is_agent:
         Agent.objects.create(user=instance)
+                             
 
-
-post_save.connect(create_client_after_user_created, sender=User)
+post_init.connect(create_client_after_user_created, sender=User)
